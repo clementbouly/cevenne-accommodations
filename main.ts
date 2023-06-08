6ea2d7144bd4615c12199b2e628eec183e0ffe30
clementbouly <clementbouly@hotmail.fr>
2023-06-08 18:27:26 +0200

    Revert "feat(images): implement lazy loading for images using IntersectionObserver"
    This reverts commit 577a32faaca33a4a38f4159674086e1d4cd7170d.

--- main.ts
+++ main.ts
@@ -2,25 +2,4 @@ import "./saas/main.scss"
 import "./components/header"
 import "./components/footer"
 
-// Lazy loading for images
-const images = document.querySelectorAll("img[data-src]")
-
-function handleIntersection(entries) {
-	entries.map((entry) => {
-		if (entry.isIntersecting) {
-			entry.target.src = entry.target.dataset.src
-			entry.target.classList.add("loaded")
-			observer.unobserve(entry.target)
-		}
-	})
-}
-
-const options = {
-	root: null,
-	rootMargin: "50%",
-	threshold: 0,
-}
-
-const observer = new IntersectionObserver(handleIntersection, options)
-
-images.forEach((image) => observer.observe(image))
+console.log("INIT JS")
